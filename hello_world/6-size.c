6f4d21a7f268078c51d9669024be43c9afdb3d80
OanaFun <7491@holbertonstudents.com>
2023-10-16 00:45:41 -0700

    3rd fix Task 6 C - Hello, World

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,10 +5,10 @@
 */
 int main(void)
 {
-printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
-printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
-printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long longint));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
