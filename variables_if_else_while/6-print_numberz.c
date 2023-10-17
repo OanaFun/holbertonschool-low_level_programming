a8661783437d03594cc7eab96cf31cb40963e99c
OanaFun <7491@holbertonstudents.com>
2023-10-17 09:03:45 -0700

    Task 6 C - Variables, if, else, while

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print all digit numbers of base 10 starting from 0
+ *
+ * Return: 0 when successful
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
