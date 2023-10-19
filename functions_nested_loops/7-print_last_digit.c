510c99fdb70ee5322d742a674dfeda22c75b166c
OanaFun <7491@holbertonstudents.com>
2023-10-19 06:18:23 -0700

    Task 7 C - Functions, nested loops

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_last_digit - print the last digit of a number
+*
+* @n: entier
+*
+* Return: the value of the last digit
+*/
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	_putchar(last_digit + '0');
+	return (last_digit);
+}
