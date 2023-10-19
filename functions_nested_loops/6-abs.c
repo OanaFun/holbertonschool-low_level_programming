ceda2b8727e486ca6d162e91d7268e376f1ad6fd
OanaFun <7491@holbertonstudents.com>
2023-10-19 05:04:20 -0700

    Task 6 C - Functions, nested loops

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - compute the absolute value of an integer
+ *
+ * @n: entier
+ *
+ * Return: the absolute value of an integer
+*/
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	return (n);
+}
