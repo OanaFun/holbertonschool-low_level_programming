70f120f1e28916cfc216da2f48615227dd176c0a
OanaFun <7491@holbertonstudents.com>
2023-10-22 13:36:37 -0700

    Task 6 C - More functions, more nested loops

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -2,6 +2,8 @@
 /**
  * print_line - draw a straight line in the terminal
  *
+ * @n: the number of times the character '_' should be printed
+ *
  * Return: 0 when successful
 */
 void print_line(int n)
