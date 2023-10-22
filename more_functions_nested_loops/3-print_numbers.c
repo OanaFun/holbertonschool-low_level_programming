7ca2e3f20220f96c449c3f3fbef8b51c4213053e
OanaFun <7491@holbertonstudents.com>
2023-10-22 09:44:35 -0700

    Task 3 C - More functions, more nested loops

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 to 9, followed by a new line
+*/
+void print_numbers(void)
+{
+	char string = '0';
+
+	while (string <= '9')
+		{
+			_putchar(string);
+			string++;
+		}
+		_putchar('\n');
+}
