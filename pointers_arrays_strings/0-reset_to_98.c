b6e0bdd399cbfac3a4df38c40d4d6d1e05f56808
OanaFun <7491@holbertonstudents.com>
2023-10-23 23:24:27 -0700

    Task 0 C - Pointers, arrays and strings

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - update the value of int parameter to 98
+ *
+ * @n: int parameter to point to
+ *
+ * Return: 0 when successful
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
