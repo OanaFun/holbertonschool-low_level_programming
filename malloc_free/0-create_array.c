484cab37bc8bfd7971e41bcde29aaaceb1409308
OanaFun <7491@holbertonstudents.com>
2023-11-09 07:30:05 -0800

    Task 0 C - malloc, free

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *array;
 
-	array = (char *)malloc(sizeof(char) * size);
+	array = malloc(sizeof(char) * size);
 	if (size == 0 || array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
