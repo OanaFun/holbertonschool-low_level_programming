85416ca9550c6d8c8baeecf572d1fb672f757466
OanaFun <7491@holbertonstudents.com>
2023-11-14 10:56:43 -0800

    Task 1 C - Structures, typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,9 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - description for a pet
- * @name: pet name
- * @age: pet age
+ * struct dog - description for a dog
+ * @name: dog's name
+ * @age: age of the dog in decimal
  * @owner: owner's name
  *
  * Description: the attributes of a dog
@@ -14,4 +14,14 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dot_t - defines a new name for type struct dog
+*/
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
