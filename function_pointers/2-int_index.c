c26b0d4b8b82597bfa8c06c1c2ae740aa0983146
OanaFun <7491@holbertonstudents.com>
2023-11-17 07:26:22 -0800

    2nd Task 2 C - Function pointers

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,14 +13,22 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
+	int (*p)(int);
+
+	p = cmp;
+
+	if (size <= 0)
+		return (-1);
 
 	if (array == NULL || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]) != 0)
-			return (i);
+		if (p(array[i]) && array[i] != 0)
+			break;
+		if (i == size - 1)
+			return (-1);
 	}
-	return (1);
+	return (i);
 }
