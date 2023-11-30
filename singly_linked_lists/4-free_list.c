16c92367d27077144cb3bbbf8d539f07f79daee4
OanaFun <7491@holbertonstudents.com>
2023-11-30 03:33:10 -0800

    Task 4 C - Singly linked lists

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -5,13 +5,17 @@
 */
 void free_list(list_t *head)
 {
-
 	list_t *tmp;
 
+	if (head == NULL)
+	{
+		return;
+	}
 	while (head != NULL)
 	{
 		tmp = head;
-		head = head->next;
+		free(tmp->str);
 		free(tmp);
+		head = head->next;
 	}
 }
