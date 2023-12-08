0af2cfbdf26493d4aa00702a56368b214f70358b
OanaFun <7491@holbertonstudents.com>
2023-12-08 09:13:55 -0800

    Task 2 C - Hash tables

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,27 +1,13 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "hash_tables.h"
 
 /**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS.
- */
-int main(void)
+ * key_index - a function that gives the index of a key
+ * @key: the key
+ * @size: the size of the array of the hash table
+ * Return: the index at which the key/value pair should be stored
+ * in the array of the hash table
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	char *s;
-	unsigned long int hash_table_array_size;
-
-	hash_table_array_size = 1024;
-	s = "cisfun";
-	printf("%lu\n", hash_djb2((unsigned char *)s));
-	printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
-	s = "Don't forget to tweet today";
-	printf("%lu\n", hash_djb2((unsigned char *)s));
-	printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
-	s = "98";
-	printf("%lu\n", hash_djb2((unsigned char *)s));
-	printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
-	return (EXIT_SUCCESS);
+	return (hash_djb2(key) % size);
 }
